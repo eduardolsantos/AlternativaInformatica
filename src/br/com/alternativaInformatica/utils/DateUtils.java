71e87b44f441a6b36a193040f0e850249f908170
eduardolsantos <eduardolsantos1@gmail.com>
2016-10-29 19:09:18 -0200

    Comentários adicionados às rotinas de DateUtils (falta formatação html).

--- src/br/com/alternativaInformatica/utils/DateUtils.java
+++ src/br/com/alternativaInformatica/utils/DateUtils.java
@@ -5,25 +5,47 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateUtils {
-	public static String getDate(String format) {
+	/**
+	 * Retorna a data atual, utilizando o formato especificado.<br>
+	 * Exemplo: "dd/MM/yyyy", "HH:mm:ss", "yyyy", etc.
+	 * @param format O formato desejado da data atual.
+	 * @return A data atual de acordo com o formato especificado.
+	 */
+	public static String getDate(final String format) {
 		DateFormat tmpDateFormat = new SimpleDateFormat(format);
 		Date tmpDate = new Date();
 		
 		return tmpDateFormat.format(tmpDate);
 	}
 	
+	/**
+	 * Retorna a data atual.
+	 * @return A data atual no formato: "dd/MM/yyyy".
+	 */
 	public static String getCurrentDate() {
 		return getDate("dd/MM/yyyy");
 	}
 	
+	/**
+	 * Retorna a data atual para ser armazenada no Banco de Dados.
+	 * @return A data atual no formato para ser armazenada no Banco de Dados.
+	 */
 	public static String getCurrentDateSql() {
-		return getDate("dd-MM-yyyy");
+		return getDate("yyyy-MM-dd");
 	}
 	
+	/**
+	 * Retorna o horário atual.
+	 * @return O horário atual: horas, minutos e segundos.
+	 */
 	public static String getCurrentTime() {
 		return getDate("HH:mm:ss");
 	}
 	
+	/**
+	 * O ano atual.
+	 * @return O ano atual, utilizando 4 dígitos.
+	 */
 	public static String getCurrentYear() {
 		return getDate("yyyy");
 	}
