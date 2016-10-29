be241f4333dfff1696a0c88ca92d884679d7f8da
eduardolsantos <eduardolsantos1@gmail.com>
2016-10-29 18:58:13 -0200

    Comentários adicionados às rotinas de FileUtils e alteração dos parâmetros das rotinas para final. (falta adicionar formatação html aos comentários)

--- src/br/com/alternativaInformatica/utils/FileUtils.java
+++ src/br/com/alternativaInformatica/utils/FileUtils.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,7 +15,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileUtils {
-	// Se não existir uma pasta no caminho especificado, ela é criada
+	/**
+	 * Verifica se o diretório especificado em caminho existe. Se não existir, ele é criado.
+	 * @param caminho O caminho a ser validado (pode ser para um arquivo).
+	 */
 	private static void validarDiretorio(String caminho) {
 		File tmpDir = new File(caminho.substring(0, caminho.lastIndexOf("\\")));
 		
@@ -23,7 +27,13 @@ public class FileUtils {
 		}
 	}
 	
-	public static void salvarArquivo(String caminho, List<String> linhas) throws IOException {
+	/**
+	 * Cria o arquivo no caminho especificado utilizando as strings da lista <i>linhas</i>.
+	 * @param caminho O caminho em que o arquivo deve ser salvo.
+	 * @param linhas As linhas que o arquivo deve conter.
+	 * @throws IOException Se ocorrer algum erro de Input ou Output.
+	 */
+	public static void salvarArquivo(final String caminho, final List<String> linhas) throws IOException {
 		File tmpFile = new File(caminho);
 		boolean tmpSalvar = false;
 		int tmpCount = 1;
@@ -52,6 +62,7 @@ public class FileUtils {
 		BufferedWriter tmpWriter = new BufferedWriter(tmpOutputWriter);
 		
 		try {
+			// Escrevendo o Arquivo
 			for (String tmpLinha : linhas) {
 				tmpWriter.write(tmpLinha);
 			}
@@ -59,8 +70,16 @@ public class FileUtils {
 			tmpWriter.close();
 		}
 	}
-		
-	public static ArrayList<String> lerArquivo(String caminho) throws IOException {
+	
+	
+	/**
+	 * Lê o arquivo indicado em caminho como um texto e retorna uma lista de Strings contendo
+	 * as linhas lidas.
+	 * @param caminho 
+	 * @return Uma lista de Strings contendo as linhas lidas do arquivo.
+	 * @throws IOException Se o arquivo indicado não existir ou ocorrer algum erro de Input/Output.
+	 */
+	public static ArrayList<String> lerArquivo(final String caminho) throws IOException {
 		ArrayList<String> tmpResult = new ArrayList<>();
 		File tmpFile = new File(caminho);
 
@@ -78,8 +97,10 @@ public class FileUtils {
 			} finally {
 				tmpReader.close();
 			}
+			
+			return tmpResult;
 		}
 		
-		return tmpResult;
+		throw new FileNotFoundException("O arquivo indicado não existe.");
 	}
 }
